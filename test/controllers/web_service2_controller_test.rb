bd6e212cfbaa4c81fee2d75a299e39e29ab547a0
Yasel Couce <yaselc@gmail.com>
2017-04-24 06:17:16 -0300

    Added test for webservice2 controller

--- test/controllers/web_service2_controller_test.rb
+++ test/controllers/web_service2_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
+require 'net/http'
 
 class WebService2ControllerTest < ActionDispatch::IntegrationTest
-  test "should get VerifyUser" do
-    # get web_service2_VerifyUser_url
-    # assert_response :success
+  test "should not verify user" do
+    base_url =  Rails.application.config.webservice2_url
+    url = URI::join(base_url, '/rest/verify_user/unknow@domain.com?format=json')
+    request = Net::HTTP::Post.new(url.path)
+    request.body = {image: ":lkjlkjkljl"}.to_json
+    response = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
+
+    #Note this test PASSES!
+    result = JSON.parse(response.body)
+    assert_equal 'No Autorizado', result["message"]
+  end
+
+  test "should verify user" do
+    base_url =  Rails.application.config.webservice2_url
+    url = URI::join(base_url, '/rest/verify_user/contacto@aimmanager.com?format=json')
+    request = Net::HTTP::Post.new(url.path)
+    request.body = {image: ":lkjlkjkljl"}.to_json
+    response = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
+
+    #Note this test PASSES!
+    result = JSON.parse(response.body)
+    assert_equal 'No Autorizado', result["message"]
   end
 
 end
